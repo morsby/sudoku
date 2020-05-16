bca7a86271482a5f17568e61df875a14cffc7263
Morsby <sigurd.morsby@gmail.com>
2020-05-16 17:07:37 +0200

    First commit - can solve sudokus

---
+++ sudoku.go
@@ -0,0 +1,202 @@
+// Package sudoku allows for generation and
+// solving of sudoku games through a back-tracking algorithm.
+//
+// Currently only supports a 9x9 grid.
+package sudoku
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type square struct {
+	value   int
+	visible bool
+	locked  bool
+}
+
+// Board is a sudoku board
+type Board [][]square
+
+// Sprint string-prints the current board prettily.
+func (b Board) Sprint() string {
+	var board strings.Builder
+	// header
+	board.WriteString("|-----|-----|-----|\n")
+	for x := 0; x < len(b); x++ {
+		board.WriteString("|")
+		for y := 0; y < len(b[0]); y++ {
+			board.WriteString(strconv.Itoa(b[y][x].value))
+			// spacing between cells
+			if y%3 == 2 {
+				board.WriteRune('|')
+			} else {
+				board.WriteRune(' ')
+			}
+		}
+		board.WriteString("\n")
+		if x%3 == 2 {
+			board.WriteString("|-----|-----|-----|\n")
+		}
+	}
+	return strings.TrimSpace(board.String())
+}
+
+// New ceates an empty Board
+func New() Board {
+	size := 9
+
+	board := make(Board, size)
+	for i := 0; i < size; i++ {
+		board[i] = make([]square, size)
+	}
+
+	return board
+}
+
+// Parse parses a slice of strings and fills the board with it.
+// Zeroes are used for unset cells.
+func (b Board) Parse(cols []string) error {
+	if len(cols) != 9 {
+		return fmt.Errorf("invalid number of cols; want 9 got %d", len(cols))
+	}
+
+	for x, col := range cols {
+		if len(col) != 9 {
+			return fmt.Errorf("invalid number of rows; want 9 length %d", len(col))
+		}
+		for y, valueRune := range col {
+			value := int(valueRune - '0')
+			if value > 0 {
+				b.Set(x, y, value, true)
+			}
+		}
+	}
+
+	return nil
+}
+
+// Set sets a value through a 0-based index (i.e. first position is [0,0])
+func (b Board) Set(x, y, value int, lock bool) (ok bool, err error) {
+	if value > 9 {
+		return false, fmt.Errorf("invalid value: %d > 9", value)
+	}
+
+	if x > len(b) || y > len(b) || x < 0 || y < 0 {
+		return false, fmt.Errorf("invalid coordinate: %d,%d", x, y)
+	}
+
+	if !b.isSafe(x, y, value) {
+		return false, nil
+	}
+
+	b[y][x] = square{value, true, lock}
+	return true, nil
+}
+
+// Unset unsets the value at a given position
+func (b Board) Unset(x, y int) (ok bool, err error) {
+	if x > len(b) || y > len(b) || x < 0 || y < 0 {
+		return false, fmt.Errorf("invalid coordinate: %d,%d", x, y)
+	}
+
+	b[y][x] = square{}
+	return true, nil
+}
+
+// isSafe checks whether it's valid to set value at position x,y.
+func (b Board) isSafe(x, y, value int) bool {
+	for i := 0; i < len(b); i++ {
+		// check col;
+		if b[y][i].value == value {
+			return false
+		}
+
+		// check row:
+		if b[i][x].value == value {
+			return false
+		}
+	}
+
+	// check small square:
+	rowBox := y / 3 * 3
+	colBox := x / 3 * 3
+	for i := rowBox; i < rowBox+3; i++ {
+		for j := colBox; j < colBox+3; j++ {
+			if b[i][j].value == value {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+// Solve solves the sudoku
+func (b Board) Solve() {
+	type move struct {
+		square int
+		value  int
+	}
+	noOfSquares := len(b) * len(b)
+
+	for sq := 0; sq < noOfSquares; sq++ {
+		for value := 1; value < 10; value++ {
+			// already has value
+			if s, _ := b.getSquare(sq); s.value > 0 {
+				break
+			}
+			// we could assign a value and break the inner loop
+			if ok, _ := b.setSquare(sq, value); ok {
+				break
+			}
+
+			// we could find no allowed value,
+			// backtracking until we can try again.
+			for value == 9 {
+				sq--
+				lastSquare, _ := b.getSquare(sq)
+				if !lastSquare.locked {
+					// increments by one when the loop restarts
+					value = lastSquare.value
+					b.unsetSquare(sq)
+				}
+			}
+		}
+	}
+}
+
+/*
+** HELPER FUNCTIONS
+ */
+// getCoord is a helper function that gets coordinates from a given square.
+func (b Board) getCoord(n int) (x, y int) {
+	col := n / len(b)
+	return col, n - (col)*len(b)
+}
+
+// setSquare is a helper function that sets a value by square number in stead of
+// coordinates.
+// As this is a helper function and is only called doing solving, it does not lock
+// cells.
+func (b Board) setSquare(n, value int) (ok bool, err error) {
+	x, y := b.getCoord(n)
+	return b.Set(x, y, value, false)
+}
+
+// unsetSquare is a helper function that unsets a square by square number in stead of
+// coordinates.
+func (b Board) unsetSquare(n int) (ok bool, err error) {
+	x, y := b.getCoord(n)
+	return b.Unset(x, y)
+}
+
+// getSquare gets a square from its number.
+func (b Board) getSquare(n int) (sq square, err error) {
+	if n >= len(b)*len(b) {
+		return square{}, fmt.Errorf("invalid square %d; larger than grid size", n)
+	}
+	x, y := b.getCoord(n)
+	return b[y][x], nil
+}
---
+++ sudoku_test.go
@@ -0,0 +1,173 @@
+package sudoku
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	b := New()
+	rows := len(b)
+	cols := len(b[0])
+	if rows != 9 || cols != 9 {
+		t.Errorf("expected board to be 9x9, is %dx%d", cols, rows)
+	}
+}
+
+func TestSprint(t *testing.T) {
+	board := New()
+	print := board.Sprint()
+	expected := strings.TrimSpace(`
+|-----|-----|-----|
+|0 0 0|0 0 0|0 0 0|
+|0 0 0|0 0 0|0 0 0|
+|0 0 0|0 0 0|0 0 0|
+|-----|-----|-----|
+|0 0 0|0 0 0|0 0 0|
+|0 0 0|0 0 0|0 0 0|
+|0 0 0|0 0 0|0 0 0|
+|-----|-----|-----|
+|0 0 0|0 0 0|0 0 0|
+|0 0 0|0 0 0|0 0 0|
+|0 0 0|0 0 0|0 0 0|
+|-----|-----|-----|
+`)
+	if print != expected {
+		t.Errorf("Board looks wrong; got \n'%v'\n; wanted \n'%v'", print, expected)
+	}
+}
+
+func TestSet(t *testing.T) {
+	board := New()
+	ok, err := board.Set(0, 2, 3, true)
+	if !ok {
+		t.Error("expected to be able to set value")
+	}
+	if err != nil {
+		t.Errorf("Did not expect an err: %v", err)
+	}
+	got := board[2][0].value
+	wanted := 3
+	if got != wanted {
+		t.Errorf("Value not set correctly; got %d; wanted %d", got, wanted)
+	}
+
+}
+
+func TestIsSafe(t *testing.T) {
+	board := New()
+
+	if board.isSafe(0, 0, 1) != true {
+		t.Error("Expected 1 to be safe to put; was not.")
+	}
+
+	board.Set(0, 0, 1, true)
+	if board.isSafe(0, 1, 1) != false {
+		t.Error("Did not expect 1 to be safe to put; was.")
+	}
+	if board.isSafe(0, 1, 2) != true {
+		t.Error("Expected 2 to be safe to put; was not.")
+	}
+
+	if board.isSafe(1, 1, 1) != false {
+		t.Error("Expected 1 to be unsafe; was not.")
+	}
+}
+
+func TestGetCoord(t *testing.T) {
+	type expect struct {
+		input  int
+		output []int
+	}
+
+	ts := []expect{
+		expect{0, []int{0, 0}},
+		expect{1, []int{0, 1}},
+		expect{8, []int{0, 8}},
+		expect{9, []int{1, 0}},
+		expect{10, []int{1, 1}},
+		expect{80, []int{8, 8}},
+	}
+	board := New()
+	for _, v := range ts {
+		x, y := board.getCoord(v.input)
+		got := []int{x, y}
+		if !reflect.DeepEqual(got, v.output) {
+			t.Errorf("Input: %v, got: %v; wanted: %v", v.input, got, v.output)
+		}
+	}
+}
+
+func TestParse(t *testing.T) {
+	board := New()
+	seed := []string{
+		"000000000",
+		"000001098",
+		"005700030",
+		"006050801",
+		"000060000",
+		"070900000",
+		"068000407",
+		"340000000",
+		"090502600",
+	}
+	board.Parse(seed)
+	expected := strings.TrimSpace(`
+|-----|-----|-----|
+|0 0 0|0 0 0|0 0 0|
+|0 0 0|0 0 1|0 9 8|
+|0 0 5|7 0 0|0 3 0|
+|-----|-----|-----|
+|0 0 6|0 5 0|8 0 1|
+|0 0 0|0 6 0|0 0 0|
+|0 7 0|9 0 0|0 0 0|
+|-----|-----|-----|
+|0 6 8|0 0 0|4 0 7|
+|3 4 0|0 0 0|0 0 0|
+|0 9 0|5 0 2|6 0 0|
+|-----|-----|-----|
+`)
+
+	b := board.Sprint()
+	if expected != b {
+		t.Error("Board is not right.")
+	}
+}
+
+func TestSolve(t *testing.T) {
+	board := New()
+	seed := []string{
+		"000080070",
+		"058030100",
+		"000000000",
+		"026000090",
+		"400000006",
+		"700029300",
+		"007000900",
+		"100203000",
+		"060000054",
+	}
+	board.Parse(seed)
+	board.Solve()
+	solved := board.Sprint()
+	expected := strings.TrimSpace(`
+|-----|-----|-----|
+|3 1 2|5 8 6|4 7 9|
+|9 5 8|4 3 7|1 6 2|
+|6 7 4|9 1 2|5 3 8|
+|-----|-----|-----|
+|5 2 6|3 7 4|8 9 1|
+|4 3 9|1 5 8|7 2 6|
+|7 8 1|6 2 9|3 4 5|
+|-----|-----|-----|
+|2 4 7|8 6 5|9 1 3|
+|1 9 5|2 4 3|6 8 7|
+|8 6 3|7 9 1|2 5 4|
+|-----|-----|-----|`)
+
+	if expected != solved {
+		t.Errorf("Solution wrong; got:\n%v\n \t\t\t\twanted: \n%v", solved, expected)
+	}
+
+}
